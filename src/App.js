fc5af8dcd0f3d8f5058074d6a98400142af1d544
Nishi <nishi001btcsai20@igdtuw.ac.in>
2021-08-11 00:24:54 +0530

    Second Draft

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
+import logo from './logo.svg';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <SortingVisualizer></SortingVisualizer>
-    </div>
+    </div>      
   );
 }
 
